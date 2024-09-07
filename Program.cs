0071d039c73283ff41dcc210b76d3c1fc06806d4
GoldMonkey711 <acnicholson1@buffs.wtamu.edu>
2024-09-07 16:46:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 4*2.5*10;
+        double y = 3*3.3;
+        double Z = x+y;
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.WriteLine("The value of Z is:");
+        Console.Write(Z);
+    }
+}
